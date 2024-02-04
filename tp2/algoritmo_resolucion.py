be62089cd5a497e4f45a8e9878d94410c5944591
Emanuel Pelozo <emanuel.pelozo@outlook.com>
2024-02-04 12:49:25 -0300

    recordando mejores resultados para no tener que buscarlos en cada iteracion

--- tp2/algoritmo_resolucion.py
+++ tp2/algoritmo_resolucion.py
@@ -2,8 +2,11 @@
 def pd_entrenamientos(si, ei):
 
     soluciones = [([0] * len(ei)) for _ in range(len(ei))]
+    mejores_ganancias_en_conjunto_dias = []
 
     for dias_disponibles_para_entrenar in range(1, len(ei) + 1):
+
+        mejor_ganancia_actual = 0
 
         for dias_seguidos_entrenando in range(0, len(si)):
 
@@ -21,7 +24,8 @@ def pd_entrenamientos(si, ei):
             if dias_seguidos_entrenando == 0:
                 if dias_disponibles_para_entrenar > 2:
                     # -3 porque el for de dias disponibles esta numerado de 1..n
-                    ganancia_anterior = max(soluciones[dias_disponibles_para_entrenar - 3])
+                    #ganancia_anterior = max(soluciones[dias_disponibles_para_entrenar - 3])
+                    ganancia_anterior = mejores_ganancias_en_conjunto_dias[dias_disponibles_para_entrenar - 3]
 
             else:
                 # -2 porque el for de dias disponibles esta numerado de 1..n
@@ -29,5 +33,10 @@ def pd_entrenamientos(si, ei):
 
             ganancia = ganancia_anterior + ganancia_para_energia_actual
             soluciones[dias_disponibles_para_entrenar-1][dias_seguidos_entrenando] = ganancia
+
+            if ganancia > mejor_ganancia_actual:
+                mejor_ganancia_actual = ganancia
+
+        mejores_ganancias_en_conjunto_dias.append(mejor_ganancia_actual)
 
     return soluciones
