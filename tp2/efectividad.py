d81d1e6bb2811323ed3fd34ded29f9887490cdf4
Agustina Fraccaro <afraccaro@fi.uba.ar>
2024-02-05 14:45:45 -0300

    algoritmo de efectividad y archivos de prueba

---
+++ tp2/efectividad.py
@@ -0,0 +1,27 @@
+import csv
+from algoritmo_resolucion import pd_entrenamientos, pd_reconstruccion
+from main import cargar_datos
+
+def efectividad(nombre_archivo):
+    EI, SI = cargar_datos("casos/efectividad/"+nombre_archivo+".csv")
+    matriz = pd_entrenamientos(SI, EI)
+    solucion = pd_reconstruccion(matriz)
+
+    with open('casos/efectividad/resoluciones/'+nombre_archivo+"_resol.txt", 'r') as file:
+        content = file.read().split(',')
+        esperado = [int(value) for value in content]
+    #aca chequear que coincida con el resultado esperado
+    if(solucion == esperado):
+        return True
+
+def main():
+    #Lista de nombres de archivos
+    archivos = ["2_elem","2_elem_v2", "5_elem", "10_elem", "15_elem"]
+    resultados = []
+    for archivo in archivos:
+        resultados.append(efectividad(archivo))
+
+    porcentaje_efectividad = (resultados.count(True) / len(resultados)) * 100
+    print(porcentaje_efectividad)
+
+main()
