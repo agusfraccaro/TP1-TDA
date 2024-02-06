3e8d7a032e2e8674b17b7097f09564f3c0afd8d6
matias-gonz <maigonzalez@fi.uba.ar>
2024-02-06 17:26:01 -0300

    Update graficos complejidad

--- tp2/complejidad_algoritmica.py
+++ tp2/complejidad_algoritmica.py
@@ -1,3 +1,4 @@
+import math
 import random
 import time
 import matplotlib.pyplot as plt
@@ -6,13 +7,16 @@ from casos.generar_casos import generar_caso
 from tp2.algoritmo_resolucion import pd_entrenamientos, pd_reconstruccion
 
 
-def generar_casos(tamanios):
-    casos = [generar_caso(tamanio) for tamanio in tamanios]
-    ei_si = []
-    for caso in casos:
-        ei_si.append((list(map(lambda x: x[0], caso)), list(map(lambda x: x[1], caso))))
+def generar_experimentos(tamanios, cantidad_experimentos):
+    experimentos = []
+    for tamanio in tamanios:
+        casos = [generar_caso(tamanio) for _ in range(cantidad_experimentos)]
+        ei_si = []
+        for caso in casos:
+            ei_si.append((list(map(lambda x: x[0], caso)), list(map(lambda x: x[1], caso))))
+        experimentos.append(ei_si)
 
-    return ei_si
+    return experimentos
 
 
 def normalizar(data):
@@ -22,11 +26,12 @@ def normalizar(data):
     return normalized_data
 
 
-def generar_grafico(tamanios, t, n_al_cuadrado, titulo, label, nombre_archivo):
+def generar_grafico(tamanios, curves, labels, titulo, nombre_archivo):
     plt.figure()
-    plt.plot(tamanios, t, marker='o', label=label, linewidth=0.5, markersize=2)
-    plt.plot(tamanios, n_al_cuadrado, marker='o', label='n^2', linewidth=0.5, markersize=2)
+    for curve, label in zip(curves, labels):
+        plt.plot(tamanios, curve, marker='o', label=label, linewidth=0.5, markersize=2)
     plt.xlabel('Tamaño del set de datos (N)')
+    plt.ylabel('Tiempo normalizado')
     plt.title(titulo)
     plt.legend()
     plt.savefig(nombre_archivo)
@@ -34,36 +39,56 @@ def generar_grafico(tamanios, t, n_al_cuadrado, titulo, label, nombre_archivo):
 
 def main():
     random.seed(42)
-
-    tamanios = range(2, 2 ** 11, 16)
-    #tamanios = range(2, 2 ** 9, 16)
-
-    casos = generar_casos(tamanios)
+    tamanios = range(2, 2 ** 10, 8)
+    experimentos = generar_experimentos(tamanios, 10)
     t_total = []
     t_entrenamientos = []
     t_reconstruccion = []
     n_al_cuadrado = normalizar([n ** 2 for n in tamanios])
-    for caso in casos:
-        start = time.time()
-        soluciones = pd_entrenamientos(caso[1], caso[0])
-        t_entrenamientos.append(time.time() - start)
+    n_lineal = normalizar([n for n in tamanios])
 
-        start_reconstruccion = time.time()
-        pd_reconstruccion(soluciones)
-        t_reconstruccion.append(time.time() - start_reconstruccion)
+    for experimento in experimentos:
+        _t_total = 0
+        _t_entrenamientos = 0
+        _t_reconstruccion = 0
+        for caso in experimento:
+            start = time.time()
+            soluciones = pd_entrenamientos(caso[1], caso[0])
+            _t_entrenamientos += time.time() - start
+            start_reconstruccion = time.time()
+            pd_reconstruccion(soluciones)
+            end = time.time()
+            _t_reconstruccion += end - start_reconstruccion
+            _t_total += end - start
 
-        t_total.append(time.time() - start)
+        t_total.append(_t_total / len(experimento))
+        t_entrenamientos.append(_t_entrenamientos / len(experimento))
+        t_reconstruccion.append(_t_reconstruccion / len(experimento))
 
     t_total = normalizar(t_total)
     t_entrenamientos = normalizar(t_entrenamientos)
     t_reconstruccion = normalizar(t_reconstruccion)
 
-    generar_grafico(tamanios, t_total, n_al_cuadrado, 'Complejidad algorítmica total', 'Tiempo de ejecución total',
-                    'graficos/complejidad_total.png')
-    generar_grafico(tamanios, t_reconstruccion, n_al_cuadrado, 'Complejidad algorítmica reconstrucción de la solución',
-                    'Tiempo de ejecución reconstrucción', 'graficos/complejidad_reconstruccion.png')
-    generar_grafico(tamanios, t_entrenamientos, n_al_cuadrado, 'Complejidad algorítmica solución',
-                    'Tiempo de ejecución entrenamientos', 'graficos/complejidad_entrenamientos.png')
+    generar_grafico(
+        tamanios,
+        [t_total, n_al_cuadrado],
+        ['Complejidad algorítmica total', 'n^2'],
+        'Tiempo de ejecución total',
+        'graficos/complejidad_total.png'
+    )
+    generar_grafico(
+        tamanios,
+        [t_entrenamientos, n_al_cuadrado],
+        ['Complejidad algorítmica entrenamientos', 'n^2'],
+        'Tiempo de ejecución entrenamientos',
+        'graficos/complejidad_entrenamientos.png'
+    )
+    generar_grafico(
+        tamanios, [t_reconstruccion, n_lineal, n_al_cuadrado],
+        ['Complejidad algorítmica reconstrucción', 'n', 'n^2'],
+        'Tiempo de ejecución reconstrucción',
+        'graficos/complejidad_reconstruccion.png'
+    )
 
 
 if __name__ == '__main__':
